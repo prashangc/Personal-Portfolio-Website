bd45379d61a4de99b86a3925dbd8f94cd008af53
prashangc <prashannagc00@gmail.com>
2021-08-04 21:31:03 +0545

    Initial Commit

---
+++ javascript/opening.js
@@ -0,0 +1,10 @@
+function a(){
+      var Email=prompt("Enter your E-mail Address");
+    if (Email=="") {
+      alert("You cannot enter");
+      return false;
+      }else{
+      document.getElementByClass("a");
+      return true;
+      }
+    }
